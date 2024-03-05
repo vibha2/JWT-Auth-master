e8ae38c29914ad920a3a9c03f79a9d0a0c9745fb
vibha2 <juhisahu2203@gmail.com>
2024-03-05 16:34:18 +0530

    navbar added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Header from './components/Header/Header';
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      Navbar
+      <Header />
+      <Routes>
+        
+      </Routes>
     </div>
   );
 }
---
+++ frontend/src/components/Header/Header.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import { FaSignInAlt, FaSignOutAlt } from 'react-icons/fa';
+
+
+const Header = () => {
+
+  return (
+<header>
+      <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+        <Container>
+          <LinkContainer to='/'>
+            <Navbar.Brand>MERN Auth</Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ms-auto'>
+              {/* {userInfo ? (
+                <>
+                  <NavDropdown title={userInfo.name} id='username'>
+                    <LinkContainer to='/profile'>
+                      <NavDropdown.Item>Profile</NavDropdown.Item>
+                    </LinkContainer>
+                    <NavDropdown.Item onClick={logoutHandler}>
+                      Logout
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                </>
+              ) : ( */}
+                <>
+                  <LinkContainer to='/login'>
+                    <Nav.Link>
+                      <FaSignInAlt />
+                       Login
+                    </Nav.Link>
+                  </LinkContainer>
+                  <LinkContainer to='/register'>
+                    <Nav.Link>
+                      <FaSignOutAlt />
+                       Signup
+                    </Nav.Link>
+                  </LinkContainer>
+                </>
+              {/* )} */}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  )
+}
+
+export default Header
