1ba91ed6f2e22a96b430b8767620641645a9100d
vibha2 <juhisahu2203@gmail.com>
2024-03-05 17:25:04 +0530

    visitor homepage done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,21 @@
 import './App.css';
 import Header from './components/Header/Header';
 import { Route, Routes } from "react-router-dom";
+import Background from './components/Background/Background';
+import Login from './components/Login/Login';
+import Register from './components/Register/Register';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Routes>
-        
-      </Routes>
+      <div className="bodyContainer">
+        <Routes>
+          <Route path="/" element={<Background/>} />
+          <Route path="/login" element={<Login/>} />
+          <Route path="/register" element={<Register/>} />
+        </Routes>
+      </div>
     </div>
   );
 }
---
+++ frontend/src/components/Background/Background.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Background.css'
+
+function Background() {
+  return (
+    <div className="backgroundBody">
+        <h2>Authentication with JWT</h2>
+        <p>
+        Empower your application with secure and seamless authentication solutions, all crafted with React at the core. Dive into a world where user authentication meets elegant user interfaces, making your application experience both intuitive and secure.
+        </p>
+    </div>
+  )
+}
+
+export default Background
